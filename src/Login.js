599387d79d577051a840518557601c3ae703699d
Cory House <housecor@gmail.com>
2019-06-05 20:15:31 -0500

    add mock api

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // this is equivalent to the code above.
+  // const emailState = useState("");
+  // const email = useState[0];
+  // const setEmail = useState[1];
+
+  function handleLogin(event) {}
+
   return (
-    <form>
+    <form onSubmit={handleLogin}>
       <div>
         <label>Email</label>
         <br />
-        <input type="text" />
+        <input
+          type="email"
+          onChange={event => setEmail(event.target.value)}
+          value={email}
+        />
       </div>
       <div>
         <label>Password</label>
         <br />
-        <input type="password" />
+        <input
+          type="password"
+          onChange={event => setPassword(event.target.value)}
+          value={password}
+        />
       </div>
       <input type="submit" value="Login" />
     </form>
