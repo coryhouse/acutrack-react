865d8d1d14a41588eeb9801aef718d1ecc747376
Cory House <housecor@gmail.com>
2019-06-20 17:28:45 -0500

    write first test

---
+++ src/utils/math.test.js
@@ -0,0 +1,13 @@
+import { add } from "./math";
+
+it("should return 3 when passed 1 and 2", () => {
+  // declare my test.
+
+  // arrange
+
+  // act
+  const result = add(1, 2);
+
+  // assert
+  expect(result).toBe(3);
+});
