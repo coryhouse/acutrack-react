8729dfde95003f6040019e7b2bf21ab75d9ab3b9
Cory House <housecor@gmail.com>
2019-06-20 16:55:21 -0500

    add reducer

---
+++ src/vehicleReducer.js
@@ -0,0 +1,29 @@
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case "increment":
+//       return { count: state.count + 1 };
+//     case "decrement":
+//       return { count: state.count - 1 };
+//     default:
+//       throw new Error();
+//   }
+// }
+
+export function vehicleReducer(state, action) {
+  switch (action.type) {
+    case "load":
+      return action.vehicles;
+    case "add":
+      return [...state, action.vehicle];
+    case "edit":
+      // Iterate over vehicles, and replace the savedVehicle in the array.
+      return state.map(vehicle =>
+        vehicle.id === action.vehicle.id ? action.vehicle : vehicle
+      );
+    case "delete":
+      return state.filter(vehicle => vehicle.id !== action.vehicleId);
+
+    default:
+      throw new Error("Unknown action type:" + action.type);
+  }
+}
