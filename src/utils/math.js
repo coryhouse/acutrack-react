422264f5cf14ff435c999d796fda697d77bb9512
Cory House <housecor@gmail.com>
2019-06-20 17:27:04 -0500

    created math util

---
+++ src/utils/math.js
@@ -0,0 +1,9 @@
+// Pure function
+// Doesn't mutate its inputs
+// Doesn't rely on global state
+// Returns a value
+// I can run this multiple times and I always get the same result
+// Easy to test
+export function add(x, y) {
+  return x + y;
+}
