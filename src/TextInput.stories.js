e6cfe16837bfffa239e7c1d8bfe05f11f859442d
Cory House <housecor@gmail.com>
2019-06-28 13:32:59 -0500

    Add storybook knobs

--- src/TextInput.stories.js
+++ src/TextInput.stories.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import TextInput from "./TextInput";
 import { storiesOf } from "@storybook/react";
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
 
 function ExampleTextInput(props) {
   const [value, setValue] = useState("");
@@ -18,6 +19,14 @@ function ExampleTextInput(props) {
 }
 
 storiesOf("TextInput", module)
-  .add("default", () => <ExampleTextInput label="Example label" />)
+  // Add the `withKnobs` decorator to add knobs support to your stories.
+  // You can also configure `withKnobs` as a global decorator.
+  .addDecorator(withKnobs)
+  .add("default", () => (
+    <ExampleTextInput
+      label={text("Label", "Example label")}
+      error={text("Error", null)}
+    />
+  ))
   .add("no label", () => <ExampleTextInput />)
   .add("with error", () => <ExampleTextInput error="Example error" />);
