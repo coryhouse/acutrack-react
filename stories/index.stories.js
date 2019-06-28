ab656bc6728b8c6cc834c36fd638edf14acded14
Cory House <housecor@gmail.com>
2019-06-28 13:12:18 -0500

    Remove moved code

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -5,7 +5,6 @@ import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
 
 import { Button, Welcome } from "@storybook/react/demo";
-import TextInput from "../src/TextInput";
 
 storiesOf("Welcome", module).add("to Storybook", () => (
   <Welcome showApp={linkTo("Button")} />
@@ -22,23 +21,3 @@ storiesOf("Button", module)
       </span>
     </Button>
   ));
-
-function ExampleTextInput(props) {
-  const [value, setValue] = useState("");
-
-  return (
-    <TextInput
-      label={props.label}
-      onChange={event => setValue(event.target.value)}
-      value={value}
-      name="example"
-      id="example"
-      error={props.error}
-    />
-  );
-}
-
-storiesOf("TextInput", module)
-  .add("default", () => <ExampleTextInput label="Example label" />)
-  .add("no label", () => <ExampleTextInput />)
-  .add("with error", () => <ExampleTextInput error="Example error" />);
